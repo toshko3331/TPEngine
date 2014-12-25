74a2b7bfb6f1a9624fe0247beed16bc22b8bbebd
toshko3331 <toshko9999@gmail.com>
2014-12-25 13:39:31 -0500

    Updated some spacing issues.

--- Software-Rendering/src/Main.cpp
+++ Software-Rendering/src/Main.cpp
@@ -68,10 +68,10 @@ int main(int argc, char ** argv)
 		SDL_RenderCopy(window.GetRenderer(), window.GetTexture(), NULL, NULL);
 	        SDL_RenderPresent(window.GetRenderer());
    	 }
-
-    SDL_DestroyTexture(window.GetTexture());
-    SDL_DestroyRenderer(window.GetRenderer());
-    SDL_DestroyWindow(window.GetWindow());
-    SDL_Quit(); 
-    return 0;
+    
+	SDL_DestroyTexture(window.GetTexture());
+	SDL_DestroyRenderer(window.GetRenderer());
+	SDL_DestroyWindow(window.GetWindow());
+	SDL_Quit(); 
+	return 0;
 }
