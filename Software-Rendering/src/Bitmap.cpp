3d4bd8eade1cfe3cd0380359f83ea573eac2238f
toshko3331 <toshko9999@gmail.com>
2014-12-25 00:08:32 -0500

    Updated the SetPixel method so it takes in x and y coordinates.

--- Software-Rendering/src/Bitmap.cpp
+++ Software-Rendering/src/Bitmap.cpp
@@ -3,7 +3,7 @@
 
 Bitmap::Bitmap(int width,int height) : m_width(width), 	m_height(height)
 {
-	//Since we are using 4 byte data types to store our pixels
+	//Since we are using 4 byte data types to store our pixels instead of my initial thought of usinc chars
 	//We don't have to do (width * height * 4) for our array decleration.
 	m_pixels = new Uint32[width * height];
 	Bitmap::Clear(128);
@@ -16,10 +16,12 @@ Bitmap::~Bitmap(){
 
 void  Bitmap::Clear(Uint32 shade)
 {
+	//Probably fastest way to set the pixels to a certain value.
 	memset(m_pixels,shade, m_width * m_height * sizeof(Uint32) );
 }
-void Bitmap::SetPixel(int index,Uint32 a,Uint32 r,Uint32 g,Uint32 b)
+
+void Bitmap::SetPixel(int x,int y,Uint32 a,Uint32 r,Uint32 g,Uint32 b)
 {
 	//Bitshifting colors into the correct positions for the ARGB format.
-	m_pixels[index] = b | (g << 8) | (r << 16) | (a << 24);
+	m_pixels[y * m_width + x] = b | (g << 8) | (r << 16) | (a << 24);
 }
--- Software-Rendering/src/Bitmap.h
+++ Software-Rendering/src/Bitmap.h
@@ -8,7 +8,7 @@ class Bitmap
 		Bitmap(int width,int height);
 		~Bitmap();
 		void Clear(Uint32 shade);
-		void SetPixel(int index,Uint32 a,Uint32 r,Uint32 g,Uint32 b);
+		void SetPixel(int x,int y,Uint32 a,Uint32 r,Uint32 g,Uint32 b);
 		Uint32* GetPixels(){ return m_pixels; } 
 	private:
 		const int m_width;
