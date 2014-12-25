83a87ee682e26e3506fa4398de19f5216c151919
toshko3331 <toshko9999@gmail.com>
2014-12-25 11:58:59 -0500

    Updated the log function so it writes to a custom file type.

--- Software-Rendering/src/ErrorReport.h
+++ Software-Rendering/src/ErrorReport.h
@@ -11,7 +11,7 @@ public:
 	static void WriteToLog(std::string error)
 	{
 		std::ofstream log;
-		log.open("ErrorLog.txt");
+		log.open("ErrorLog.TPEngineLog");
 		log << error << " error: " << SDL_GetError() << std::endl;
 		std::cout << error << " error: " << SDL_GetError() << std::endl;
 		log.close();
