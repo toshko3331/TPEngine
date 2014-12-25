9883dc73322cc9ddcdff8e471ba902d7271f5ef1
toshko3331 <toshko9999@gmail.com>
2014-12-25 00:11:39 -0500

    Added the testing code for the rasterizer.

--- Software-Rendering/src/Main.cpp
+++ Software-Rendering/src/Main.cpp
@@ -10,8 +10,8 @@
 //
 #include <iostream>
 #include "HeadersInclude.h"
-#define WIDTH 1600
-#define HEIGHT 900
+#define WIDTH 640 
+#define HEIGHT 400 
 
 
 int main(int argc, char ** argv)
@@ -24,22 +24,34 @@ int main(int argc, char ** argv)
 		ErrorReport::WriteToLog("SDL_Init");
 		return 1;
 	}
-
+		
 	Window window = Window("Software Renderer",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,0);
-
+	
 	Bitmap pixels = Bitmap(window.GetWindowWidth(),window.GetWindowHeight());
 	bool quit = false;
 	SDL_Event event;
-	
+	int i = 0; // Temporary 'i' variable for testing code.
+	Rasterizer rasterizer = Rasterizer();
 	while (!quit)
-    {	
-		
+	{	
+	
 		//Logic of this loop
 		//------------------
 		//1.Events
 		//2.Logic
 		//3.Rendering
-
+		//Just some test code for the Horizontal Line drawing.
+		if(i < HEIGHT && i%2 == 0)
+		{
+			rasterizer.DrawHorizontalLine(Vertex(Vector4f(0,i,0,0)),Vertex(Vector4f(WIDTH,i,0,0)),&pixels);
+		}
+		else if(i >= HEIGHT)
+		{
+			SDL_Delay(2000);
+			quit = true;
+		}
+		i++;
+		//End of test code.
 		//1.Events
 		while(SDL_PollEvent(&event))
 		{
@@ -52,15 +64,16 @@ int main(int argc, char ** argv)
 
 		//3.Rendering
 		SDL_UpdateTexture(window.GetTexture(), NULL ,pixels.GetPixels(),WIDTH * 4 * sizeof(char));
-        SDL_RenderClear(window.GetRenderer());
+	        SDL_RenderClear(window.GetRenderer());
 		SDL_RenderCopy(window.GetRenderer(), window.GetTexture(), NULL, NULL);
-        SDL_RenderPresent(window.GetRenderer());
-    }
+	        SDL_RenderPresent(window.GetRenderer());
+   	 }
 
     SDL_DestroyTexture(window.GetTexture());
+    ErrorReport::WriteToLog("DestroyWindow");
     SDL_DestroyRenderer(window.GetRenderer());
     SDL_DestroyWindow(window.GetWindow());
     SDL_Quit();
-
+    ErrorReport::WriteToLog("DestroyWindow");
     return 0;
 }
