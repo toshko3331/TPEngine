886882cbed79325eaffdec5fffa90d69cc9c7f36
toshko3331 <toshko9999@gmail.com>
2014-12-27 02:45:44 -0500

    Edge class used for rasterizing triangles.

--- Software-Rendering/src/Edge.cpp
+++ Software-Rendering/src/Edge.cpp
@@ -2,8 +2,11 @@
 #include <math.h>
 Edge::Edge(Vertex minYVertex,Vertex maxYVertex)
 {
+	//To get the starting x we use the formula ((x2 - x1)/(y2 - y1)) + x1  
+
 	m_yBegin = ceil(minYVertex.GetY());
 	m_yEnd = ceil(maxYVertex.GetY());
+	//The amount by which we step our x.
 	m_reciprocalSlope =  (maxYVertex.GetX() - minYVertex.GetX()) / (maxYVertex.GetY() - minYVertex.GetY()); 
 	
 	// This variable is created because we need to take into account the truncated
@@ -11,16 +14,12 @@ Edge::Edge(Vertex minYVertex,Vertex maxYVertex)
 	// If we don't this can come and haunt us later in the program at unexpected times.
 	float yApproxFloatError = minYVertex.GetY() - m_yBegin; 
 	m_x = (m_reciprocalSlope * yApproxFloatError) + minYVertex.GetX();
-
-	m_xCeiling = ceil(m_x);
 }
 
 void Edge::Step()
 {
 	//Step all of our gradients here.
 	m_x = m_x + m_reciprocalSlope;
-	m_xCeiling = ceil(m_x);
-	
 	
 }
 
--- Software-Rendering/src/Edge.h
+++ Software-Rendering/src/Edge.h
@@ -9,13 +9,10 @@ class Edge
 		float GetX(){return m_x;}
 		int GetYStart(){return m_yBegin;}
 		int GetYEnd(){return m_yEnd;}
-		int GetXCeiling(){return m_xCeiling;}
 		float GetReciprocalSlope(){return m_reciprocalSlope;} 
 	private:
 		int m_yBegin;
 		int m_yEnd;
-
 		float m_x;
-		float m_xCeiling;
 		float m_reciprocalSlope;
 };
