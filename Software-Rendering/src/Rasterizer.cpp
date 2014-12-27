6a0c01a8037ceae8e53bc198d0d8c7d68d359fb4
toshko3331 <toshko9999@gmail.com>
2014-12-27 02:50:32 -0500

    Finalized the rasterizer, removed the debug code, and added comments.

--- Software-Rendering/src/Rasterizer.cpp
+++ Software-Rendering/src/Rasterizer.cpp
@@ -1,13 +1,19 @@
 #include "HeadersInclude.h"
 #include <math.h>
+//Temporary includes for the test code.
+#include <time.h>
+//End of temporary includes.
 Rasterizer::Rasterizer(Bitmap* bitmap)
 {
 	m_bitmap = bitmap;
+	//Seeding the RNG for the test code.
+	srand(time(NULL));
+	//End of test code.
 }
 
 void Rasterizer::RasterizeTriangle(Vertex minYVertex,Vertex midYVertex,Vertex maxYVertex)
 {
-	 //Sort the vertices
+	 //Sorting the vertices.
 	 if(minYVertex.GetY() > midYVertex.GetY())
 	 {
 		Vertex temp = minYVertex;
@@ -28,78 +34,105 @@ void Rasterizer::RasterizeTriangle(Vertex minYVertex,Vertex midYVertex,Vertex ma
 		minYVertex = midYVertex;
 	 	midYVertex = temp;
 	 }
-	///Major Debug Code///
-	std::cout << "(" << minYVertex.GetX() << "," << minYVertex.GetY() << ")" <<std::endl;
-	std::cout << "(" << midYVertex.GetX() << "," << midYVertex.GetY() << ")" <<std::endl;
-	std::cout << "(" << maxYVertex.GetX() << "," << maxYVertex.GetY() << ")" <<std::endl;
-	///End Debug Code////
+	//Creating the edges on which the triangle will be based upon.	
 	Edge topToBottom = Edge(minYVertex,maxYVertex);
 	Edge topToMiddle = Edge(minYVertex,midYVertex);
 	Edge middleToBottom = Edge(midYVertex,maxYVertex);
-	///Major Debug Code///
-//	std::cout << topToMiddle.GetX() << "," << topToMiddle.GetYEnd() <<std::endl;
-	std::cout << topToBottom.GetYStart() << "," << topToBottom.GetYEnd() <<std::endl;
-//	std::cout << middleToBottom.GetX() << "," << middleToBottom.GetYEnd() <<std::endl;
-	///End Debug Code////
-	//Add handedness.	
-	ScanEdge(topToBottom,topToMiddle,true,false);//,minYVertex * maxYVertex);
-	ScanEdge(topToBottom,middleToBottom,true,true);//,midYVertex * maxYVertex);
-			
+	//Test code for funzies. :P
+	Vector4f color = Vector4f(rand()%255,rand()%255,rand()%255,rand()%255);
+	//End of test code.
+	//Check weather the longest side is on the right or left.
+	//This determines the function it is going to use to draw the specific triangle. 
+	if(topToBottom.GetX() < middleToBottom.GetX())
+	{
+		//This function call is for drawing the top triangle.
+		LeftLongestEdge(topToBottom,topToMiddle,false,color);
+		//This function call is for drawing the bottom part of the triangle.
+		LeftLongestEdge(topToBottom,middleToBottom,true,color);
+	}
+	else
+	{
+		//This function call is for drawing the top triangle.
+		RightLongestEdge(topToMiddle,topToBottom,false,color);
+		//This function call is for drawing the bottom part of the triangle.
+		RightLongestEdge(middleToBottom,topToBottom,true,color);
+	}
 }
 
-void Rasterizer::ScanEdge(Edge left,Edge right,bool IsRightEdgeLongerThanLeft,bool secondTriangle)//,Vector4f handedness)
-{
-	//TODO: make sure that the edges are in the correct order.
-		
-	if(IsRightEdgeLongerThanLeft)
-	{
-		Edge temp = left;
-		left = right;
-		right = temp;
+void Rasterizer::RightLongestEdge(Edge left,Edge right,bool secondTriangle,Vector4f color)
+{	
+	//yBegin = The point on the Y-Axis at which we are going to start drawing the triangle.
+	int yBegin = right.GetYStart();
+	//yEnd = The point on the Y-Axis at which we are going to stop drawing the triangle.
+	int yEnd = left.GetYEnd();
+	
+	//The algorithm is based on drawing the bottom and top triangles sepratly.
+	//It is necessary to do a few things different if it is drawing the bottom half of the triangle.
+	if(secondTriangle){
+		//Set the beginning of the y-axis to the appropriate value.
+		//This is done by simply getting the midYVertex's Y positon since that is the beginning
+		//of the bottom part of the triangle.
+		yBegin = left.GetYStart();
+		//Setting the proper y-axis end of the 2nd triangle.
+		yEnd = right.GetYEnd();
+		//Making sure that the x value of the longest side is stepped so it is
+		//at the position that starts the bottom part of the triangle.
+		for(int i = right.GetYStart();i < left.GetYStart();i++)
+		{
+			right.Step();	
+		}
 	}
 	
-	int yBegin = left.GetYStart();
-//	std::cout << yBegin << "\t" << yEnd << std::endl;
-//	int yBegin = 0;
-        int yEnd = left.GetYEnd();
-//	std::cout << left.GetX() <<  std::endl;
-//	std::cout << right.GetX() << std::endl;
-//	std::cout << "//////" <<std::endl;
-	std::cout << left.GetReciprocalSlope() << "\t" << right.GetReciprocalSlope() << std::endl;
-	if(secondTriangle){
-	for(int i = right.GetYStart();i <= yBegin;i++)
+	for(int currentY = yBegin;currentY < yEnd;currentY++)
 	{
-		right.Step();
-		
-		std::cout << "Am I even here?" << std::endl;
-	}
-	}
-	for(int currentY = yBegin;currentY <= yEnd;currentY++)
-	{
-		DrawScanLine(left.GetX(),right.GetX(),currentY);	
+		DrawScanLine(left,right,currentY,color);	
 		left.Step();
 		right.Step();
-	}
-	
-//	std::cout << "Right.x after loop " << right.GetX() << std::endl;
+	}	
 }
 
-void Rasterizer::DrawScanLine(int min,int max,int currentY)
+void Rasterizer::LeftLongestEdge(Edge left,Edge right,bool secondTriangle,Vector4f color)
 {
-	/*
+	
+	/*									*
+	 *	REFFER TO ABOVE FUNCTION FOR COMMENTS AND BRIEF EXPLANATIONS	*
+	 *									*/
+
+	int yBegin = left.GetYStart();
+	int yEnd = right.GetYEnd();
+	
+	if(secondTriangle)
+	{
+		yBegin = right.GetYStart();
+		yEnd = left.GetYEnd();
+		
+		for(int i = left.GetYStart();i < right.GetYStart();i++)
+		{
+			left.Step();
+		}
+	}
+	for(int currentY = yBegin;currentY < yEnd;currentY++)
+	{
+		DrawScanLine(left,right,currentY,color);	
+		//Increment our values so we can step along the edge.
+		left.Step();
+		right.Step();
+	}	
+}
+
+void Rasterizer::RasterizeHorizontalLine(Edge left,Edge right,int currentY,Vector4f color)
+{
+
+	//These 2 variables can be thought of as limits. 
+	//xBegin sets the minimum and xEnd sets the maximum values for the x axis.
+	//These are the values at which a pixel should be filled
 	int xBegin = ceil(left.GetX());
 	int xEnd = ceil(right.GetX());
-	*/
-
-	int xBegin = min;
-	int xEnd = max;
-
-	//std::cout << xBegin << "\t" << xEnd << std::endl;	
-	for(int currentX = xBegin;currentX <= xEnd;currentX++)
+	
+	for(int currentX = xBegin;currentX < xEnd;currentX++)
 	{
-		m_bitmap->SetPixel(currentX,currentY,0,0,0,0);
+		//The x values at whcih to draw the pixel is decided by the minimum and maximum variables
+		//while the Y-Axis is decided from the function from which this function is called.
+		m_bitmap->SetPixel(currentX,currentY,color.GetX(),color.GetY(),color.GetZ(),color.GetW());
 	}
 }
-
-
-
--- Software-Rendering/src/Rasterizer.h
+++ Software-Rendering/src/Rasterizer.h
@@ -1,15 +1,16 @@
 #pragma once
 #include "HeadersInclude.h"
 
-
 class Rasterizer 
 {
 	public:
 		Rasterizer(Bitmap* bitmap);
 		//Error checking of the order of the Verticies being passed in should be done by the user of the function
 		void RasterizeTriangle(Vertex maxYVertex,Vertex midYVertex,Vertex minYVertex);
-		void ScanEdge(Edge left,Edge right,bool IsRightEdgeLongerThanLeft, bool secondTriangle);//,Vector4f handedness);
-		void DrawScanLine(int min,int max,int currentY);
+		void LeftLongestEdge(Edge left,Edge right,bool secondTriangle,Vector4f color);
+		void RightLongestEdge(Edge left,Edge right,bool secondTriangle,Vector4f color);
+		void RasterizeHorizontalLine(Edge left,Edge right,int currentY,Vector4f color);
+
 	private:
 		Bitmap* m_bitmap;
 	
