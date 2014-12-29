bea6de67d53347440fc1b24aa20428b5d3371c40
toshko3331 <toshko9999@gmail.com>
2014-12-29 02:12:09 -0500

    Fixed bugs, and implemented the transformation functions properly.

--- Software-Rendering/src/Matrix4f.cpp
+++ Software-Rendering/src/Matrix4f.cpp
@@ -6,25 +6,25 @@
 
 Matrix4f::Matrix4f()
 {
-	//Sets the matrix to the identity matrix.
-	m_matrix[0][0] = 1;	m_matrix[1][0] = 0;	m_matrix[2][0] = 0;	m_matrix[3][0] = 0;
-	
-	m_matrix[0][1] = 0;	m_matrix[1][1] = 1;	m_matrix[2][1] = 0;	m_matrix[3][1] = 0;
-	
-	m_matrix[0][2] = 0;	m_matrix[1][2] = 0;	m_matrix[2][2] = 1;	m_matrix[3][2] = 0;
-	
-	m_matrix[0][3] = 0;	m_matrix[1][3] = 0;	m_matrix[2][3] = 0;	m_matrix[3][3] = 1;
+
+}
+
+Matrix4f Matrix4f::InitializeIdentity()
+{
+	return (Matrix4f(
+	1,	0,	0,	0,
+	0,	1,	0,	0,
+	0,	0,	1,	0,
+	0,	0,	0,	1
+	));
 }
 
 Matrix4f::Matrix4f(float one, float two, float three, float four, float five ,float six, float seven, float eight, float nine, float ten, float eleven, float twelve, float thirteen, float fourteen, float fifteen, float sixteen)
 {
 	m_matrix[0][0] = one;		m_matrix[1][0] = two;		m_matrix[2][0] = three;		m_matrix[3][0] = four;
-	
 	m_matrix[0][1] = five;		m_matrix[1][1] = six;		m_matrix[2][1] = seven;		m_matrix[3][1] = eight;
-	
 	m_matrix[0][2] = nine;		m_matrix[1][2] = ten;		m_matrix[2][2] = eleven;	m_matrix[3][2] = twelve;
-	
-	m_matrix[0][3] = twelve;	m_matrix[1][3] = thirteen;	m_matrix[2][3] = fifteen;	m_matrix[3][3] = sixteen;
+	m_matrix[0][3] = thirteen;	m_matrix[1][3] = fourteen;	m_matrix[2][3] = fifteen;	m_matrix[3][3] = sixteen;
 }
 
 Matrix4f Matrix4f::operator*(const float scalar)
@@ -32,41 +32,38 @@ Matrix4f Matrix4f::operator*(const float scalar)
 	//Multiplies everything by a scalar.
 	return Matrix4f(
 	m_matrix[0][0] * scalar,	m_matrix[1][0] * scalar,	m_matrix[2][0] * scalar,	m_matrix[3][0] * scalar,
-	
 	m_matrix[0][1] * scalar,	m_matrix[1][1] * scalar,	m_matrix[2][1] * scalar,	m_matrix[3][1] * scalar,
-	
 	m_matrix[0][2] * scalar,	m_matrix[1][2] * scalar,	m_matrix[2][2] * scalar,	m_matrix[3][2] * scalar,
-	
 	m_matrix[0][3] * scalar,	m_matrix[1][3] * scalar,	m_matrix[2][3] * scalar,	m_matrix[3][3] * scalar
 	);
 }
 
-Matrix4f Matrix4f::operator*(Matrix4f matrix2)
+Matrix4f Matrix4f::operator*(const Matrix4f matrix2)
 {
 	return Matrix4f(
 	//First row of the new matrix.
-	(m_matrix[0][0] * matrix2.Get(0,0)) + (m_matrix[0][0] * matrix2.Get(0,1)) + (m_matrix[0][0] * matrix2.Get(0,2)) + (m_matrix[0][0] * matrix2.Get(0,3)), 
-	(m_matrix[1][0] * matrix2.Get(1,0)) + (m_matrix[1][0] * matrix2.Get(1,1)) + (m_matrix[1][0] * matrix2.Get(1,2)) + (m_matrix[1][0] * matrix2.Get(1,3)), 
-	(m_matrix[2][0] * matrix2.Get(2,0)) + (m_matrix[2][0] * matrix2.Get(2,1)) + (m_matrix[2][0] * matrix2.Get(2,2)) + (m_matrix[2][0] * matrix2.Get(2,3)), 
-	(m_matrix[3][0] * matrix2.Get(3,0)) + (m_matrix[3][0] * matrix2.Get(3,1)) + (m_matrix[3][0] * matrix2.Get(3,2)) + (m_matrix[3][0] * matrix2.Get(3,3)), 
+	(m_matrix[0][0] * matrix2.Get(0,0)) + (m_matrix[1][0] * matrix2.Get(0,1)) + (m_matrix[2][0] * matrix2.Get(0,2)) + (m_matrix[3][0] * matrix2.Get(0,3)), 
+	(m_matrix[0][0] * matrix2.Get(1,0)) + (m_matrix[1][0] * matrix2.Get(1,1)) + (m_matrix[2][0] * matrix2.Get(1,2)) + (m_matrix[3][0] * matrix2.Get(1,3)), 
+	(m_matrix[0][0] * matrix2.Get(2,0)) + (m_matrix[1][0] * matrix2.Get(2,1)) + (m_matrix[2][0] * matrix2.Get(2,2)) + (m_matrix[3][0] * matrix2.Get(2,3)), 
+	(m_matrix[0][0] * matrix2.Get(3,0)) + (m_matrix[1][0] * matrix2.Get(3,1)) + (m_matrix[2][0] * matrix2.Get(3,2)) + (m_matrix[3][0] * matrix2.Get(3,3)), 
 	//Second row of the new matrix.
-	(m_matrix[0][1] * matrix2.Get(0,0)) + (m_matrix[0][1] * matrix2.Get(0,1)) + (m_matrix[0][1] * matrix2.Get(0,2)) + (m_matrix[0][1] * matrix2.Get(0,3)), 
-	(m_matrix[1][1] * matrix2.Get(1,0)) + (m_matrix[1][1] * matrix2.Get(1,1)) + (m_matrix[1][1] * matrix2.Get(1,2)) + (m_matrix[1][1] * matrix2.Get(1,3)), 
-	(m_matrix[2][1] * matrix2.Get(2,0)) + (m_matrix[2][1] * matrix2.Get(2,1)) + (m_matrix[2][1] * matrix2.Get(2,2)) + (m_matrix[2][1] * matrix2.Get(2,3)), 
-	(m_matrix[3][1] * matrix2.Get(3,0)) + (m_matrix[3][1] * matrix2.Get(3,1)) + (m_matrix[3][1] * matrix2.Get(3,2)) + (m_matrix[3][1] * matrix2.Get(3,3)), 
+	(m_matrix[0][1] * matrix2.Get(0,0)) + (m_matrix[1][1] * matrix2.Get(0,1)) + (m_matrix[2][1] * matrix2.Get(0,2)) + (m_matrix[3][1] * matrix2.Get(0,3)), 
+	(m_matrix[0][1] * matrix2.Get(1,0)) + (m_matrix[1][1] * matrix2.Get(1,1)) + (m_matrix[2][1] * matrix2.Get(1,2)) + (m_matrix[3][1] * matrix2.Get(1,3)), 
+	(m_matrix[0][1] * matrix2.Get(2,0)) + (m_matrix[1][1] * matrix2.Get(2,1)) + (m_matrix[2][1] * matrix2.Get(2,2)) + (m_matrix[3][1] * matrix2.Get(2,3)), 
+	(m_matrix[0][1] * matrix2.Get(3,0)) + (m_matrix[1][1] * matrix2.Get(3,1)) + (m_matrix[2][1] * matrix2.Get(3,2)) + (m_matrix[3][1] * matrix2.Get(3,3)), 
 	//Third row of the new matrix.
-	(m_matrix[0][2] * matrix2.Get(0,0)) + (m_matrix[0][2] * matrix2.Get(0,1)) + (m_matrix[0][2] * matrix2.Get(0,2)) + (m_matrix[0][2] * matrix2.Get(0,3)), 
-	(m_matrix[1][2] * matrix2.Get(1,0)) + (m_matrix[1][2] * matrix2.Get(1,1)) + (m_matrix[1][2] * matrix2.Get(1,2)) + (m_matrix[1][2] * matrix2.Get(1,3)), 
-	(m_matrix[2][2] * matrix2.Get(2,0)) + (m_matrix[2][2] * matrix2.Get(2,1)) + (m_matrix[2][2] * matrix2.Get(2,2)) + (m_matrix[2][2] * matrix2.Get(2,3)), 
-	(m_matrix[3][2] * matrix2.Get(3,0)) + (m_matrix[3][2] * matrix2.Get(3,1)) + (m_matrix[3][2] * matrix2.Get(3,2)) + (m_matrix[3][2] * matrix2.Get(3,3)), 
+	(m_matrix[0][2] * matrix2.Get(0,0)) + (m_matrix[1][2] * matrix2.Get(0,1)) + (m_matrix[2][2] * matrix2.Get(0,2)) + (m_matrix[3][2] * matrix2.Get(0,3)), 
+	(m_matrix[0][2] * matrix2.Get(1,0)) + (m_matrix[1][2] * matrix2.Get(1,1)) + (m_matrix[2][2] * matrix2.Get(1,2)) + (m_matrix[3][2] * matrix2.Get(1,3)), 
+	(m_matrix[0][2] * matrix2.Get(2,0)) + (m_matrix[1][2] * matrix2.Get(2,1)) + (m_matrix[2][2] * matrix2.Get(2,2)) + (m_matrix[3][2] * matrix2.Get(2,3)), 
+	(m_matrix[0][2] * matrix2.Get(3,0)) + (m_matrix[1][2] * matrix2.Get(3,1)) + (m_matrix[2][2] * matrix2.Get(3,2)) + (m_matrix[3][2] * matrix2.Get(3,3)), 
 	//Fourth row of the new matrix.
-	(m_matrix[0][3] * matrix2.Get(0,0)) + (m_matrix[0][3] * matrix2.Get(0,1)) + (m_matrix[0][3] * matrix2.Get(0,2)) + (m_matrix[0][3] * matrix2.Get(0,3)), 
-	(m_matrix[1][3] * matrix2.Get(1,0)) + (m_matrix[1][3] * matrix2.Get(1,1)) + (m_matrix[1][3] * matrix2.Get(1,2)) + (m_matrix[1][3] * matrix2.Get(1,3)), 
-	(m_matrix[2][3] * matrix2.Get(2,0)) + (m_matrix[2][3] * matrix2.Get(2,1)) + (m_matrix[2][3] * matrix2.Get(2,2)) + (m_matrix[2][3] * matrix2.Get(2,3)), 
-	(m_matrix[3][3] * matrix2.Get(3,0)) + (m_matrix[3][3] * matrix2.Get(3,1)) + (m_matrix[3][3] * matrix2.Get(3,2)) + (m_matrix[3][3] * matrix2.Get(3,3))
+	(m_matrix[0][3] * matrix2.Get(0,0)) + (m_matrix[1][3] * matrix2.Get(0,1)) + (m_matrix[2][3] * matrix2.Get(0,2)) + (m_matrix[3][3] * matrix2.Get(0,3)), 
+	(m_matrix[0][3] * matrix2.Get(1,0)) + (m_matrix[1][3] * matrix2.Get(1,1)) + (m_matrix[2][3] * matrix2.Get(1,2)) + (m_matrix[3][3] * matrix2.Get(1,3)), 
+	(m_matrix[0][3] * matrix2.Get(2,0)) + (m_matrix[1][3] * matrix2.Get(2,1)) + (m_matrix[2][3] * matrix2.Get(2,2)) + (m_matrix[3][3] * matrix2.Get(2,3)), 
+	(m_matrix[0][3] * matrix2.Get(3,0)) + (m_matrix[1][3] * matrix2.Get(3,1)) + (m_matrix[2][3] * matrix2.Get(3,2)) + (m_matrix[3][3] * matrix2.Get(3,3))
 	);
 }
-
+/* This might be wrong, going to fix it later.
 Vector4f Matrix4f::operator*(Vector4f vector)
 {
 	return Vector4f( (m_matrix[0][0] * vector.GetX()) + (m_matrix[0][1] * vector.GetY()) + (m_matrix[0][2] * vector.GetZ()) + (m_matrix[0][3] * vector.GetW()),
@@ -75,47 +72,117 @@ Vector4f Matrix4f::operator*(Vector4f vector)
 			 (m_matrix[3][0] * vector.GetX()) + (m_matrix[3][1] * vector.GetY()) + (m_matrix[3][2] * vector.GetZ()) + (m_matrix[3][3] * vector.GetW())
 			 );
 }
-
-void Matrix4f::Scale(Vector3f vector)
+*/
+Matrix4f Matrix4f::Translate(Vector3f vector)
 {
-	m_matrix[0][0] = m_matrix[0][0] * vector.GetX();
+	Matrix4f translatedMatrix = Matrix4f().InitializeIdentity(); 
 
-	m_matrix[1][1] = m_matrix[1][1] * vector.GetY();
+	translatedMatrix.Set(3,0,vector.GetX());
+	translatedMatrix.Set(3,1,vector.GetY());
+	translatedMatrix.Set(3,2,vector.GetZ());
+
+	return ((*this) * translatedMatrix);
+}
+
+Matrix4f Matrix4f::Scale(Vector3f vector)
+{
+	Matrix4f scaledMatrix = Matrix4f().InitializeIdentity();
+
+	scaledMatrix.Set(0,0,vector.GetX());
+	scaledMatrix.Set(1,1,vector.GetY());
+	scaledMatrix.Set(2,2,vector.GetZ());
+
+	return ((*this) * scaledMatrix);
+}
+
+Matrix4f Matrix4f::RotateAroundX(float angleInDegrees)
+{
+	float angleInRadians = angleInDegrees * PI / 180;	
+	float cosOfAngle = cos(angleInRadians);
+	float sinOfAngle = sin(angleInRadians);
+
+	Matrix4f rotatedMatrix = Matrix4f().InitializeIdentity();
+	//Set the rotation matracies to the needed values 
+	rotatedMatrix.Set(1,1,cosOfAngle);	rotatedMatrix.Set(2,1,-sinOfAngle);
+	rotatedMatrix.Set(1,2,sinOfAngle);	rotatedMatrix.Set(2,2,cosOfAngle);
+	return ((*this) * rotatedMatrix);
+
+}
+
+Matrix4f Matrix4f::RotateAroundY(float angleInDegrees)
+{
+	float angleInRadians = angleInDegrees * PI / 180;	
+	float cosOfAngle = cos(angleInRadians);
+	float sinOfAngle = sin(angleInRadians);
+
+	Matrix4f rotatedMatrix = Matrix4f().InitializeIdentity();
 	
-	m_matrix[2][2] = m_matrix[2][2] * vector.GetZ();
-}
-
-void Matrix4f::RotateAroundX(float angleInDegrees)
-{
-	float angleInRadians = angleInDegrees * PI / 180;	
-	float cosOfAngle = cos(angleInRadians);
-	float sinOfAngle = sin(angleInRadians);
+	//Set the rotation matracies to the needed values 
+	rotatedMatrix.Set(0,0,cosOfAngle);	rotatedMatrix.Set(2,0,sinOfAngle);
+	rotatedMatrix.Set(0,2,-sinOfAngle);	rotatedMatrix.Set(2,2,cosOfAngle);
 	
-	m_matrix[1][1] = m_matrix[1][1] * cosOfAngle;	m_matrix[2][1] = m_matrix[2][1] * -sinOfAngle;
-	m_matrix[1][2] = m_matrix[1][2] * sinOfAngle;	m_matrix[2][2] = m_matrix[2][2] * cosOfAngle;
+	return ((*this) * rotatedMatrix);
 }
 
-void Matrix4f::RotateAroundY(float angleInDegrees)
+Matrix4f Matrix4f::RotateAroundZ(float angleInDegrees)
 {
 	float angleInRadians = angleInDegrees * PI / 180;	
 	float cosOfAngle = cos(angleInRadians);
 	float sinOfAngle = sin(angleInRadians);
 
-	m_matrix[0][0] = m_matrix[0][0] * cosOfAngle;	m_matrix[2][0] = m_matrix[2][0] * sinOfAngle;
-	m_matrix[0][2] = m_matrix[0][2] * -sinOfAngle;	m_matrix[2][2] = m_matrix[2][2] * cosOfAngle;
+	Matrix4f rotatedMatrix = Matrix4f().InitializeIdentity();
+	//Set the rotation matracies to the needed values 
+	rotatedMatrix.Set(0,0,cosOfAngle);	rotatedMatrix.Set(1,0,-sinOfAngle);
+	rotatedMatrix.Set(0,1,sinOfAngle);	rotatedMatrix.Set(1,1,cosOfAngle);
+	
+	return ((*this) * rotatedMatrix);
 }
-
-void Matrix4f::RotateAroundZ(float angleInDegrees)
+float** Matrix4f::GetMatrix()
 {
-	float angleInRadians = angleInDegrees * PI / 180;	
-	float cosOfAngle = cos(angleInRadians);
-	float sinOfAngle = sin(angleInRadians);
+	float** matrix = new float*[4];// = new float*;
+	
+	for(int i = 0; i < 4; i++)
+	{
+		matrix[i] = new float[4];
+		for(int j = 0; j < 4; j++)
+		{
+			matrix[i][j] = m_matrix[i][j];	
+		}
 
-	m_matrix[0][0] = m_matrix[0][0] * cosOfAngle;	m_matrix[1][0] = m_matrix[1][0] * -sinOfAngle;
-	m_matrix[0][1] = m_matrix[0][1] * sinOfAngle;	m_matrix[1][1] = m_matrix[1][1] * cosOfAngle;
+	}
+	// THIS VARIABLE IS NOT DELETED ANYWHERE!!! This is the reason why this function is private.
+	// Because AssignMatrix should delete it whcih is the only function that uses this function.
+	// If any others use it, make sure that we delete this variable.
+	return matrix;
 }
 
+void Matrix4f::AssignMatrix(float** matrixBeingAssigned)
+{
+	for(size_t i = 0;i < 4;i++)
+	{
+		for(size_t j = 0;j < 4;j++)
+		{
+			m_matrix[i][j] = matrixBeingAssigned[i][j];
+		}
+	}
+	delete[] matrixBeingAssigned;
+}
 
+void Matrix4f::Set(int column,int row,float value)
+{
+	m_matrix[column][row] = value;
+}
+
+void Matrix4f::PrintMatrixToConsole()
+{
+	std::cout << "--- "	<< 		     "\t"  <<		       "\t" 		    <<   "\t"		        << "---" << std::endl;
+	std::cout << "| "	<< m_matrix[0][0] << "\t" << m_matrix[1][0] << "\t" << m_matrix[2][0] << "\t" << m_matrix[3][0] << "  |" << std::endl;
+	std::cout << "| " 	<< m_matrix[0][1] << "\t" << m_matrix[1][1] << "\t" << m_matrix[2][1] << "\t" << m_matrix[3][1] <<"   |" << std::endl;
+	std::cout << "| "	<< m_matrix[0][2] << "\t" << m_matrix[1][2] << "\t" << m_matrix[2][2] << "\t" << m_matrix[3][2] <<"   |" << std::endl; 
+	std::cout << "| "	<< m_matrix[0][3] << "\t" << m_matrix[1][3] << "\t" << m_matrix[2][3] << "\t" << m_matrix[3][3] <<"   |" << std::endl;
+	std::cout << "--- "	<< 		     "\t"  <<		       "\t" 		    <<   "\t"		        << "---" << std::endl;
+	
+}
 
 
 
--- Software-Rendering/src/Matrix4f.h
+++ Software-Rendering/src/Matrix4f.h
@@ -8,16 +8,22 @@ class Matrix4f
 		Matrix4f(float one, float two, float three, float four, float five ,float six, float seven, float eight, float nine, float ten, float eleven, float twelve, float thirteen, float fourteen, float fifteen, float sixteen);
 		//Operator overloading
 		Matrix4f operator*(const float scalar);
-		Matrix4f operator*(Matrix4f matrix2);
-		Vector4f operator*(Vector4f vector);
+		Matrix4f operator*(const Matrix4f matrix2);
+		//Vector4f operator*(Vector4f vector); Might be wrong, check later.
 		
 		//Getters
-		float Get(short column, short row){return m_matrix[column][row];}
-		//Method
-		void Scale(Vector3f vector);
-		void RotateAroundX(float angleInDegrees);
-		void RotateAroundY(float angleInDegrees);
-		void RotateAroundZ(float angleInDegrees);
-	private:
+		float Get(int column,int row) const {return m_matrix[column][row];} 
+		//Methods
+		Matrix4f InitializeIdentity();
+		Matrix4f Scale(Vector3f vector);
+		Matrix4f Translate(Vector3f vector);
+		Matrix4f RotateAroundX(float angleInDegrees);
+		Matrix4f RotateAroundY(float angleInDegrees);
+		Matrix4f RotateAroundZ(float angleInDegrees);
+		void PrintMatrixToConsole();	
+		private:
+		float** GetMatrix(); // This function might be obsolete.
+		void Set(int column,int row,float value);
+		void AssignMatrix(float** matrixBeingAssigned);// Make sure that we are deleteing this variable correctly. This function might be obsolete.
 		float m_matrix[4][4];
 };
