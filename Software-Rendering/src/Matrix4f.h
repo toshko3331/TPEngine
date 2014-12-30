2a3c01d7d16d3f782c2579a236fc98aa6a8d3551
toshko3331 <toshko9999@gmail.com>
2014-12-29 23:58:44 -0500

    Fixed the prototype for perspective projection.

--- Software-Rendering/src/Matrix4f.h
+++ Software-Rendering/src/Matrix4f.h
@@ -20,7 +20,7 @@ class Matrix4f
 		Matrix4f RotateAroundX(float angleInDegrees);
 		Matrix4f RotateAroundY(float angleInDegrees);
 		Matrix4f RotateAroundZ(float angleInDegrees);
-		Matrix4f PerspectiveProjection(int fov, float zNear,float zFar);
+		Matrix4f PerspectiveProjection(int fov,float aspectRatio, float zNear,float zFar);
 		Matrix4f OrthographicProjection(int width, int height, float zFar, float zNear);
 		void PrintMatrixToConsole();	
 		private:
