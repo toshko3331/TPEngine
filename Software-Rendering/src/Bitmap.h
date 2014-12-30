b2beb369d1b6994834bba6774839bb03797b7855
toshko3331 <toshko9999@gmail.com>
2014-12-30 03:29:50 -0500

    Added a GetWidth and a GetHeight method that returns the bitmap's width and height respectively.

--- Software-Rendering/src/Bitmap.h
+++ Software-Rendering/src/Bitmap.h
@@ -9,6 +9,8 @@ class Bitmap
 		~Bitmap();
 		void Clear(Uint32 shade);
 		void SetPixel(int x,int y,Uint32 a,Uint32 r,Uint32 g,Uint32 b);
+		int GetWidth() { return m_width;}
+		int GetHeight() { return m_height;}
 		Uint32* GetPixels(){ return m_pixels; } 
 	private:
 		const int m_width;
