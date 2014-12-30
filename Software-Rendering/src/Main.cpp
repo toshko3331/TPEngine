d469151ce12b96e98da4752a938b54dbf32f06c2
toshko3331 <toshko9999@gmail.com>
2014-12-30 17:59:39 -0500

    Added the chrono class for acquiring the delta. This might be used to aquire an fps counter later on.

--- Software-Rendering/src/Main.cpp
+++ Software-Rendering/src/Main.cpp
@@ -3,6 +3,7 @@
 //Temporary include calls
 #include <time.h>
 #include <math.h>
+#include <chrono> // This might not be a good choice since it might not be supported across all compilers.
 //End of temporary include calls
 
 #define WIDTH 1200 
@@ -29,18 +30,24 @@ int main(int argc, char ** argv)
 	Vertex v1 = Vertex(Vector4f(0,1,0,1));
 	Vertex v2 = Vertex(Vector4f(-1,-1,0,1));
 	Vertex v3 = Vertex(Vector4f(1,-1,0,1));
-	int i = 0;
 	srand(time(NULL));
 	
 	Rasterizer rasterizer = Rasterizer(&pixels);
-	Matrix4f projection = Matrix4f().InitializeIdentity().PerspectiveProjection(90,pixels.GetWidth()/pixels.GetHeight(),0.1,1000);
+	Matrix4f projection = Matrix4f().InitializeIdentity().PerspectiveProjection(70,pixels.GetWidth()/pixels.GetHeight(),0.1,1000);
 	Matrix4f translation = Matrix4f().InitializeIdentity().Translate(Vector3f(0,0,3));
 	projection = projection * translation;
-	projection.PrintToConsole();
+	//projection.PrintToConsole();
 	//// End of Temporary Variables ///
+	
+	//Used for delta counter.
+	auto lastTime = std::chrono::high_resolution_clock::now();
+	float rotAmount = 0.0;
 	while (!quit)
 	{	
-
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		auto delta = (std::chrono::duration_cast<std::chrono::nanoseconds>(currentTime-lastTime).count())/1000000;
+		lastTime = currentTime;
+		
 		SDL_UpdateTexture(window.GetTexture(), NULL ,pixels.GetPixels(),WIDTH * sizeof(Uint32));	
 		//Logic of this loop
 		//------------------
@@ -51,15 +58,15 @@ int main(int argc, char ** argv)
 		///	v1 = Vertex(Vector4f(rand()%WIDTH,rand()%HEIGHT,0,0));
 		///	v2 = Vertex(Vector4f(rand()%WIDTH,rand()%HEIGHT,0,0));
 		///	v3 = Vertex(Vector4f(rand()%WIDTH,rand()%HEIGHT,0,0));
-			
-			Matrix4f rotation = Matrix4f().InitializeIdentity().RotateAroundY(0.5 * i);
-			rotation = projection * rotation;
-			//projection.PrintToConsole();
-			pixels.Clear(128);		
-		
-			rasterizer.RasterizeTriangle(v1.ApplyTransformations(rotation),v2.ApplyTransformations(rotation),v3.ApplyTransformations(rotation));
-		i++;	
-
+				
+		Matrix4f rotation = Matrix4f().InitializeIdentity().RotateAroundY(rotAmount * 0.09);
+		rotation = projection * rotation;
+		//projection.PrintToConsole();
+		pixels.Clear(128);		
+	
+		rasterizer.RasterizeTriangle(v1.ApplyTransformations(rotation),v2.ApplyTransformations(rotation),v3.ApplyTransformations(rotation));
+	
+		rotAmount = rotAmount + delta;
 		//End of test code.
 		//1.Events
 		while(SDL_PollEvent(&event))
@@ -75,7 +82,9 @@ int main(int argc, char ** argv)
 	        SDL_RenderClear(window.GetRenderer());
 		SDL_RenderCopy(window.GetRenderer(), window.GetTexture(), NULL, NULL);
 	        SDL_RenderPresent(window.GetRenderer());
-   	 }
+       	}
+
+
 	 
 	SDL_DestroyTexture(window.GetTexture());
 	SDL_DestroyRenderer(window.GetRenderer());
