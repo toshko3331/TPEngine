01c3394d94f1ef0ed0841ac14a176a1df5f55953
toshko3331 <toshko9999@gmail.com>
2015-01-01 20:50:04 -0500

    Added the texel coordinates as a paramter for the Vertex constructor.

--- Software-Rendering/src/Vertex.cpp
+++ Software-Rendering/src/Vertex.cpp
@@ -1,20 +1,23 @@
 #include "HeadersInclude.h"
-Vertex::Vertex(Vector4f vertex)
+Vertex::Vertex(Vector4f vertex,Vector2f texel)
 {
 	m_x = vertex.GetX();
 	m_y = vertex.GetY();
 	m_z = vertex.GetZ();
 	m_w = vertex.GetW();
+	
+	m_u = texel.GetX();
+	m_v = texel.GetY();
 }
 
 Vertex Vertex::PerspectiveDivide()
 {
-	return Vertex(Vector4f(m_x/m_w,m_y/m_w,m_z/m_w,m_w/m_w));
+	return Vertex(Vector4f(m_x/m_w,m_y/m_w,m_z/m_w,m_w/m_w),Vector2f(m_u,m_v));
 }
 
 Vertex Vertex::ApplyTransformations(Matrix4f transformations)
 {	
-	Vertex newVertex = Vertex(transformations * this->GetVertex());
+	Vertex newVertex = Vertex(transformations * this->GetVertex(),Vector2f(m_u,m_v));
 	return newVertex;
 }
 
--- Software-Rendering/src/Vertex.h
+++ Software-Rendering/src/Vertex.h
@@ -4,14 +4,15 @@
 class Vertex 
 {
 	public:
-		Vertex(Vector4f vertex);
+		Vertex(Vector4f vertex,Vector2f texel);
 		//Getters.
 		float GetX() { return m_x;}
 		float GetY() { return m_y;}
 		float GetZ() { return m_z;}
 		float GetW() { return m_w;}
-		//Vector4f GetVertex(){ return m_vertex;}
 		Vector4f GetVertex(){ return Vector4f(m_x,m_y,m_z,m_w);}
+		float	 GetU(){ return m_u;}
+		float	 GetV(){ return m_v;}
 		//Methods
 		Vertex PerspectiveDivide();
 		Vertex ApplyTransformations(Matrix4f transformations);
@@ -21,4 +22,7 @@ class Vertex
 		float m_y;
 		float m_z;
 		float m_w;
+
+		float m_u;
+		float m_v;
 };
