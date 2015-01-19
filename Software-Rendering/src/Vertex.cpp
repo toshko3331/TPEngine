a364c9d8dd3dc6f9393e0ba451683fec31d84052
toshko3331 <toshko9999@gmail.com>
2015-01-19 16:56:31 -0500

    Apperently I though that I should be dividing m_w by itself... This was causing everything to be rendered in a non-perspective correct fashion. Fixed.

--- Software-Rendering/src/Vertex.cpp
+++ Software-Rendering/src/Vertex.cpp
@@ -12,13 +12,13 @@ Vertex::Vertex(Vector4f vertex,Vector2f texel)
 
 Vertex Vertex::PerspectiveDivide()
 {
-	return Vertex(Vector4f(m_x/m_w,m_y/m_w,m_z/m_w,m_w/m_w),Vector2f(m_u,m_v));
+	return Vertex(Vector4f(m_x/m_w,m_y/m_w,m_z/m_w,m_w),Vector2f(m_u,m_v));
 }
 
 Vertex Vertex::ApplyTransformations(Matrix4f transformations)
 {	
-	Vertex newVertex = Vertex(transformations * this->GetVertex(),Vector2f(m_u,m_v));
-	return newVertex;
+	return Vertex(transformations * this->GetVertex(),Vector2f(m_u,m_v));
+	
 }
 
 void Vertex::PrintToConsole(bool withEndLineAppended)
