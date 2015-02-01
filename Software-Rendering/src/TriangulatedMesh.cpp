0336e550d36f869d00aa1439093b8114a4819c03
Tomas <toshko9999@gmail.com>
2015-02-01 13:18:37 -0500

    Removed some dev comments and updated the Vector2f that was being assigned into the Vertex. Now it counts according to the bubble sort algorithm.

--- Software-Rendering/src/TriangulatedMesh.cpp
+++ Software-Rendering/src/TriangulatedMesh.cpp
@@ -7,11 +7,6 @@ TriangulatedMesh::TriangulatedMesh(Object* object)  : m_rawVertecies(object->Get
 		//ErrorWriteToLog("Model could not be loaded.");
 		return;
 	}
-	//Faces can be stored as Vector3f
-	//for each facex
-	//	store the vertecies in the order which they should be drawn in	
-	//
-	//Each face can be a vector3 triplet
 
 	for(unsigned int i = 0; i < object->GetVertexVector().size()/4;i++)
 	{
@@ -19,16 +14,7 @@ TriangulatedMesh::TriangulatedMesh(Object* object)  : m_rawVertecies(object->Get
 		const std::vector<float>& texelVector = object->GetTexelVector();
 		m_objectVertecies.push_back( Vertex(
 			Vector4f(vertexVector.at(i+1),vertexVector.at(i+2),vertexVector.at(i+3),1),
-			Vector2f(texelVector.at(i),texelVector.at(i+1))));
+			Vector2f(texelVector.at(i+1),texelVector.at(i+2))));
 	}
-/*
-	//This function actually might not be even needed.
-	for(int i = 0;i <= model.GetAmountOfFaces; i++)
-		verteciesInOrder[i] = faces.GetVertex(i + 0)
-		verteciesInOrder[i + 1] = faces.GetVertex(i + 1)
-		verteciesInOrder[i + 2] = faces.GetVertex(i + 2)
-	
-	amountOfTriangles = AmountOfFaces;
-*/
 }
 
