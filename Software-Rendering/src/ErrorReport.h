f676d9bb2b14e73c89137f61ee563ed6c489be8a
Tomas <toshko9999@gmail.com>
2015-02-09 19:21:36 -0500

    Added a new function and renamed old one to accomadate for the new naming convention.

--- Software-Rendering/src/ErrorReport.h
+++ Software-Rendering/src/ErrorReport.h
@@ -8,12 +8,21 @@
 class ErrorReport
 {
 public:
+	static void WriteToLog_SDL(std::string error)
+	{
+		std::ofstream log;
+		log.open("ErrorLog_SDL.TPEngineLog");
+		log << error << " error: " << SDL_GetError() << std::endl;
+		std::cout << error << " error: " << SDL_GetError() << std::endl;
+		log.close();
+	}
+
 	static void WriteToLog(std::string error)
 	{
 		std::ofstream log;
 		log.open("ErrorLog.TPEngineLog");
-		log << error << " error: " << SDL_GetError() << std::endl;
-		std::cout << error << " error: " << SDL_GetError() << std::endl;
+		log << "Error: " << error << std::endl;
+		std::cout << "Error: " << error << std::endl;
 		log.close();
 	}
 };
