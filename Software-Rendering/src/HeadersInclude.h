80536825c6a6485cda84bbc1c8e51b7b9a28d52e
Tomas <toshko9999@gmail.com>
2015-02-13 22:10:04 -0500

    Added the Camera class header and rearanged the other includes in a more logical fashion.

--- Software-Rendering/src/HeadersInclude.h
+++ Software-Rendering/src/HeadersInclude.h
@@ -8,14 +8,15 @@
 #include <SDL\SDL.h>
 #endif
 //Doing it this way so we can include everything with just one header file.
-#include "Bitmap.h"
-#include "ErrorReport.h"
-#include "Vector3f.h"
-#include "Window.h"
 #include "Vector4f.h"
-#include "Matrix4f.h"
 #include "Vector2f.h"
+#include "Vector3f.h"
+#include "Matrix4f.h"
+#include "ErrorReport.h"
+#include "Window.h"
+#include "Bitmap.h"
 #include "Vertex.h"
+#include "Camera.h"
 #include "Gradients.h"
 #include "Edge.h"
 #include "Object.h"
