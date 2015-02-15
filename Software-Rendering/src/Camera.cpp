aa1e7890364b88ee3fbcfdda817deb7649d73eb7
Tomas <toshko9999@gmail.com>
2015-02-14 20:40:23 -0500

    Prototype implementation of the camera.

--- Software-Rendering/src/Camera.cpp
+++ Software-Rendering/src/Camera.cpp
@@ -1,31 +1,113 @@
 #include "HeadersInclude.h"
 
-
 //Perspective camera.
-Camera::Camera(int fov,float aspectRatio,float zNear,float zFar,Matrix4f transformation ) : m_cameraMatrix(Matrix4f().InitializeIdentity().PerspectiveProjection(fov,aspectRatio,zNear,zFar))
+Camera::Camera(int screenWidth,int fov,float aspectRatio,float zNear,float zFar,Matrix4f transformation ) : m_projectionMatrix(Matrix4f().InitializeIdentity().PerspectiveProjection(fov,aspectRatio,zNear,zFar))
 {
-	m_cameraMatrix = m_cameraMatrix * transformation;
-	m_typeOfProjection = 0;
+	m_xRotation = 0;
+	m_yRotation = 0;
+	m_screenWidth = screenWidth;
+	//TODO:Do arithmetic with the transofrmation matrix to the rotation and coordinate vectors.
+
 }
 
+
 //Orthographic camera.
-Camera::Camera(int width, int height, float zFar, float zNear,Matrix4f transformation) : m_cameraMatrix(Matrix4f().InitializeIdentity().OrthographicProjection(width,height,zFar,zNear))
+//TODO:Implement this properly.
+Camera::Camera(int width, int height, float zFar, float zNear,Matrix4f transformation) : m_projectionMatrix(Matrix4f().InitializeIdentity().OrthographicProjection(width,height,zFar,zNear))
 {
-	m_cameraMatrix = m_cameraMatrix * transformation;
-	m_typeOfProjection = 1;
+
+	m_xRotation = 0;
+	m_yRotation = 0;
+	//TODO:Do arithemtic with the transformation matrix tot he rotation and coordiante vectors.
+
+}
+
+Matrix4f Camera::GetMatrix()
+{
+	//TODO:When quaternions are implemented, move the translation as the last transformation of the matrix being multiplied.
+	return m_projectionMatrix * Matrix4f().InitializeIdentity().Translate(m_position).RotateAroundX(m_rotation.GetX()).RotateAroundY(m_rotation.GetY()).RotateAroundZ(m_rotation.GetZ());
 }
 
 void Camera::UpdateCamera(SDL_Event* event)
 {
-	if (event->type == SDL_KEYDOWN)
+	if(event->type == SDL_MOUSEMOTION)
 	{
-		if (event->key.keysym.sym == SDLK_LEFT)
-        	{
-			m_cameraMatrix = m_cameraMatrix.RotateAroundY(10 * 0.9);
-		}
-		if(event->key.keysym.sym == SDLK_RIGHT)
+		float sensitivity = -0.785;
+/*		//Rotation in the X-Axis
+		float camPitch = sensitivity * (m_screenHalfHeight - event->motion.x);
+		//Rotation among the Y-Axis	
+		float camYaw = sensitivity * (m_screenHalfWidth - event->motion.y);
+
+		m_rotation = Vector3f(camYaw,camPitch,0);	
+*/
+		Sint32 currentX = event->motion.x;
+		Sint32 currentY = event->motion.y;
+		Sint32 xDifference = currentX - m_previousX;
+		Sint32 yDifference = currentY - m_previousY;
+
+		m_xRotation += xDifference;
+		m_yRotation += yDifference;
+
+		std::cout << "X: " << currentX << "Y: " << currentY << std::endl;
+
+		if(yDifference < 0 && xDifference < 0){
+			m_rotation = Vector3f(m_rotation.GetX() + -1 * sensitivity,m_rotation.GetY() + -1 * sensitivity,0);	
+		}else if(yDifference < 0 && xDifference > 0)
+			m_rotation = Vector3f(m_rotation.GetX() + -1 * sensitivity,m_rotation.GetY() + 1 * sensitivity,0);	
+	 	else if(yDifference > 0 && xDifference < 0)
+			m_rotation = Vector3f(m_rotation.GetX() + 1 * sensitivity,m_rotation.GetY() + -1 * sensitivity,0);
+		else if(yDifference > 0 && xDifference < 0)
+			m_rotation = Vector3f(m_rotation.GetX() + 1 * sensitivity,m_rotation.GetY() + 1 * sensitivity,0);	
+		else if(yDifference == 0 && xDifference > 0)
+			m_rotation = Vector3f(m_rotation.GetX(),m_rotation.GetY() + 1 * sensitivity,0);	
+	 	else if(yDifference == 0 && xDifference < 0)
+			m_rotation = Vector3f(m_rotation.GetX(),m_rotation.GetY() + -1 * sensitivity,0);
+		else if(yDifference > 0 && xDifference == 0)
+			m_rotation = Vector3f(m_rotation.GetX() + 1 * sensitivity,m_rotation.GetY(),0);
+		else if(yDifference < 0 && xDifference == 0)
+			m_rotation = Vector3f(m_rotation.GetX() + -1 * sensitivity,m_rotation.GetY(),0);
+		else if(yDifference == 0 && xDifference == 0)
+			m_rotation = Vector3f(m_rotation.GetX(),m_rotation.GetY(),0);
+		//These following if statments allow us to keep spining infinetly with the mouse.	
+		if(currentX == 0)
 		{
-			m_cameraMatrix = m_cameraMatrix.RotateAroundY(-10 * 0.9);
+			currentX = m_screenWidth - 2;
+		}else if(currentX == m_screenWidth-1)
+		{
+			currentX = 1;
 		}
+		//TODO:Eventually restrain the Y-Rotation as well.
+		m_previousX = currentX;
+		m_previousY = currentY;
+	
+	}else if (event->type == SDL_KEYDOWN)
+	{
+
+		//Not working yet properly. We are going to need Quaternions first.
+		float speedFactor = 0.2;
+		if(event->key.keysym.sym == SDLK_w)
+        	{
+			m_position = Vector3f(m_position.GetX(),m_position.GetY(),m_position.GetZ() + speedFactor);
+		}
+		if(event->key.keysym.sym == SDLK_a)
+		{
+			m_position = Vector3f(m_position.GetX() + speedFactor,m_position.GetY(),m_position.GetZ());
+		}
+		if(event->key.keysym.sym == SDLK_s)
+		{
+			m_position = Vector3f(m_position.GetX(),m_position.GetY(),m_position.GetZ() - speedFactor);
+		}
+		if(event->key.keysym.sym == SDLK_d)
+		{
+			m_position = Vector3f(m_position.GetX() - speedFactor,m_position.GetY(),m_position.GetZ());
+		}	
+		if(event->key.keysym.sym == SDLK_UP)
+		{
+		//	m_cameraMatrix = m_cameraMatrix.Translate(Vector3f(0,0,0.3));
+		}
+		if(event->key.keysym.sym == SDLK_DOWN)
+		{
+		//	m_cameraMatrix = m_cameraMatrix.Translate(Vector3f(0,0,-0.3));
+		}	
 	}
 }
--- Software-Rendering/src/Camera.h
+++ Software-Rendering/src/Camera.h
@@ -4,17 +4,22 @@
 class Camera{
 	public:
 		//Perspective camera.
-		Camera(int fov,float aspectRatio,float zNear,float zFar,Matrix4f transformation = Matrix4f().InitializeIdentity());
+		Camera(int screenWidth,int fov,float aspectRatio,float zNear,float zFar,Matrix4f transformation = Matrix4f().InitializeIdentity());
 		//Orthographic camera.
 		Camera(int width, int height, float zFar, float zNear,Matrix4f transformation = Matrix4f().InitializeIdentity());
 		void UpdateCamera(SDL_Event* event);
 		//Getters
-		Matrix4f& GetMatrix() { return m_cameraMatrix; }
-		int GetTypeOfProjection() { return m_typeOfProjection; 
-					//Returns 0 if Perspective
-					//Returns 1 if Orthographic 
-					}
+		Matrix4f GetMatrix();
 	private:
-		Matrix4f m_cameraMatrix;
-		int m_typeOfProjection;	
+		
+		int m_screenWidth;
+		Matrix4f m_projectionMatrix;
+		Sint32 m_previousX;
+		Sint32 m_previousY;
+		
+		float m_xRotation;
+		float m_yRotation;
+
+		Vector3f m_rotation = Vector3f(0,0,0);
+		Vector3f m_position = Vector3f(0,0,5);
 };
