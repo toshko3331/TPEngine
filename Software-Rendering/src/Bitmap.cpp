ccd83088b216fd474c49d9d93288e47c2990d19f
Tomas <toshko9999@gmail.com>
2015-02-16 16:21:56 -0500

    Added a texture loading constructor for png files that uses the libpng library.

--- Software-Rendering/src/Bitmap.cpp
+++ Software-Rendering/src/Bitmap.cpp
@@ -9,6 +9,87 @@ Bitmap::Bitmap(int width,int height) : m_width(width), 	m_height(height)
 	Bitmap::Clear(128);
 }
 
+Bitmap::Bitmap(std::string filePath) : m_width(GetImageDimension(filePath,true)), m_height(GetImageDimension(filePath,false))
+{	
+	unsigned char pngSigniture[8];
+	//Opening the file in binary reading mode.
+	FILE* texture = fopen(filePath.c_str(), "rb");
+	if(!texture)
+	{
+		ErrorReport::WriteToLog(filePath.append(" failed to be loaded."));
+		return;
+	}
+	fread(pngSigniture,1,8,texture);
+	//Checking if the file is of type PNG.
+	if(png_sig_cmp(pngSigniture,0,8))
+	{
+		ErrorReport::WriteToLog(filePath.append(" is not a PNG type file."));
+		return;
+	}
+	//Intialize the first struct.
+	png_structp pngReadStruct = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL,NULL,NULL);
+	if(pngReadStruct == NULL)
+	{
+		ErrorReport::WriteToLog(std::string("Read structure for ").append(filePath).append(" was not initalized correctly."));
+		return;
+	}
+	//Intialize the second struct.
+	png_infop pngInfoStruct = png_create_info_struct(pngReadStruct);
+	if(pngInfoStruct == NULL)
+	{
+		ErrorReport::WriteToLog(std::string("Info structure for ").append(filePath).append(" was not initalized correctly."));
+		return;
+	}
+	//Any I/O problem call this.
+	if(setjmp(png_jmpbuf(pngReadStruct)))
+	{
+		ErrorReport::WriteToLog(std::string("Error during I/O of file ").append(filePath));
+		return;
+	}
+	//Initalizing standard C I/O stream
+	png_init_io(pngReadStruct,texture);
+	//Sets the amount of bytes to be read as the signature.
+	png_set_sig_bytes(pngReadStruct,8);
+	//Reading information before we get to the good stuff, the image data.
+	png_read_info(pngReadStruct,pngInfoStruct);
+	
+	//Allocating the space needed to store the pixels.
+	m_pixels = new Uint32[m_width * m_height];
+
+	//Checking if the image was changed in any way since we started reading.
+	png_read_update_info(pngReadStruct,pngInfoStruct);
+	
+	//We malloc(allocate) all of the necessarry memory for the texture.
+	png_bytep* row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * m_height);
+	for(int y = 0; y < m_height; y++)
+	{
+		row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(pngReadStruct,pngInfoStruct));
+	}
+	
+	//Read the image into our 2 dimensional array.
+	png_read_image(pngReadStruct, row_pointers);
+
+        for (int y = 0;y < m_height; y++) 
+	{
+		int j = 0;
+                for (int x = 0;x < m_width * 4;x+=4)
+		{	
+			//Bit shifting according to our ARGB format. Since the image is stored as RGBA, we need to get the 4th element first (the alpha) because we are using ARGB. 
+			//This is why it might look slightly out of order.
+			m_pixels[y * m_width + j] =  (int)row_pointers[y][x + 3] << 24  | (int)row_pointers[y][x + 0] << 16 | (int)row_pointers[y][x + 1] << 8 | (int)row_pointers[y][x + 2] << 0;
+			j++;
+                }
+        }
+
+	//Make sure to always clean up malloc
+        for (int i = 0;i < m_height; i++)
+	{
+                free(row_pointers[i]);
+	}        
+	free(row_pointers);
+	fclose(texture);
+}
+
 Bitmap::~Bitmap(){
 	//Clearing memory.
 	delete[] m_pixels;
@@ -30,3 +111,60 @@ void Bitmap::CopyTexelToPixel(int texelXCoord,int texelYCoord,int pixelXCoord,in
 {
 	m_pixels[pixelYCoord * m_width + pixelXCoord] = texture->GetPixel((texelYCoord * texture->GetWidth()) + texelXCoord);
 }
+
+//Static member function to get the dimensions of the image.
+int Bitmap::GetImageDimension(std::string filePath,bool getWidth)
+{
+	/* For commented version, refer to the texture constructor. */
+	unsigned char pngSigniture[8];
+	FILE* texture = fopen(filePath.c_str(), "rb");
+	if(!texture)
+	{
+		ErrorReport::WriteToLog(filePath.append(" failed to be loaded during dimension gathering."));
+		return -1;
+	}
+	fread(pngSigniture,1,8,texture);
+	if(png_sig_cmp(pngSigniture,0,8))
+	{
+		ErrorReport::WriteToLog(filePath.append(" is was not recognized as a PNG during dimension gathering."));
+		return -1;
+	}
+	if(!texture)
+	{
+		ErrorReport::WriteToLog(filePath.append(" failed to be loaded during dimension gathering."));
+		return -1;
+	}
+	png_structp pngReadStruct = png_create_read_struct(PNG_LIBPNG_VER_STRING,NULL,NULL,NULL);
+	if(pngReadStruct == NULL)
+	{
+		ErrorReport::WriteToLog(std::string("Read structure for ").append(filePath).append(" during dimension gathering was not initalized correctly."));
+		return -1;
+	}
+	png_infop pngInfoStruct = png_create_info_struct(pngReadStruct);
+	if(pngInfoStruct == NULL)
+	{
+		ErrorReport::WriteToLog(std::string("Info structure for ").append(filePath).append(" during dimension gathering was not initalized correctly."));
+		return -1;
+	}
+	if(setjmp(png_jmpbuf(pngReadStruct)))
+	{
+		ErrorReport::WriteToLog(std::string("Error during I/O of file ").append(filePath).append(" during dimension gathering."));
+		return -1;
+	}
+	png_init_io(pngReadStruct,texture);
+	png_set_sig_bytes(pngReadStruct,8);
+	png_read_info(pngReadStruct,pngInfoStruct);
+	if(getWidth == true)
+	{
+		int width = png_get_image_height(pngReadStruct,pngInfoStruct);
+		fclose(texture);
+		return width;
+	}else
+	{
+		int height = png_get_image_height(pngReadStruct,pngInfoStruct);
+		fclose(texture);
+		return height;
+	}
+}
+
+
--- Software-Rendering/src/Bitmap.h
+++ Software-Rendering/src/Bitmap.h
@@ -6,15 +6,17 @@ class Bitmap
 {
 	public:
 		Bitmap(int width,int height);
+		Bitmap(std::string filePath);
 		~Bitmap();
 		void Clear(Uint32 shade);
 		void SetPixel(int x,int y,Uint32 a,Uint32 r,Uint32 g,Uint32 b);
 		void CopyTexelToPixel(int texelXCoord,int texelYCoord,int pixelXCoord,int pixelYCoord,Bitmap* texture);
-		int GetPixel(int location){return m_pixels[location]; } 
+		int GetPixel(int location){ return m_pixels[location]; } 
 		int GetWidth() { return m_width;}
 		int GetHeight() { return m_height;}
 		Uint32* GetPixels(){ return m_pixels; } 
 	private:
+		static int GetImageDimension(std::string filePath,bool getWidth);
 		const int m_width;
 		const int m_height;
 		Uint32* m_pixels;
