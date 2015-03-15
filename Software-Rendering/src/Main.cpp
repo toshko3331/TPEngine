0d833054b17b05c13cacc80aec6061913503ce54
Tomas <toshko9999@gmail.com>
2015-03-15 19:29:06 -0400

    Updated to use the new camera system.

--- Software-Rendering/src/Main.cpp
+++ Software-Rendering/src/Main.cpp
@@ -16,12 +16,11 @@ int main(int argc, char ** argv)
 		return 1;
 	}
 		
-	Window window = Window("Software Renderer",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,0);
+	Window window = Window("Inaniloquent Lamprophony",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,0);
 
 	Bitmap pixels = Bitmap(window.GetWindowWidth(),window.GetWindowHeight());
 	bool quit = false;
 	SDL_Event event;
-	//// Temporary Variables for Testing Code  ///	
 	//Load the level and it's objects.
 	OBJLevel level("Cube.obj");
 	//Load all of the textures.
@@ -32,48 +31,53 @@ int main(int argc, char ** argv)
 	//Get the objects from the level.
 	std::vector<Object*>& objects = level.GetObjects();
 	Rasterizer rasterizer = Rasterizer(&pixels,textures,level);
-	//// End of Temporary Variables ///
-	Camera camera(WIDTH,70,(float)pixels.GetWidth()/pixels.GetHeight(),0.1,1000,Matrix4f().InitializeIdentity().Translate(Vector3f(0,0,3)));
-	
-	//We might not need this method anymore	
-	if(SDL_SetRelativeMouseMode(SDL_TRUE) != 0)
-	{
-		ErrorReport::WriteToLog_SDL("SDL_SetRelativeMouseMode");
-	}
+
+	unsigned int currentTime = SDL_GetTicks();
+	unsigned int lastTime = SDL_GetTicks();
+	float deltaTime = currentTime - lastTime;
+
+	Camera camera(70.0f,(float)(pixels.GetWidth()/pixels.GetHeight()),0.1f,1000.0f,&deltaTime);
 	SDL_ShowCursor(0);
+
+	const float HALF_WIDTH = (float)(WIDTH/2);
+	const float HALF_HEIGHT = (float)(HEIGHT/2);
+
 	while (!quit)
-	{	
+	{
+		currentTime = SDL_GetTicks();	
 		SDL_UpdateTexture(window.GetTexture(), NULL ,pixels.GetPixels(),WIDTH * sizeof(Uint32));	
 		//Logic of this loop
 		//------------------
 		//1.Events
 		//2.Logic
 		//3.Rendering
-		pixels.Clear(128);		
-		for(unsigned int i = 0;i < objects.size();i++)
-		{
-			rasterizer.RasterizeObjMesh(camera.GetMatrix(),objects.at(i));
-		}
-		rasterizer.ClearZBuffer();
+		pixels.Clear(128);	
+		
 		//1.Events
 		while(SDL_PollEvent(&event))
 		{
-			if( event.type == SDL_QUIT )
+			if(event.type == SDL_QUIT)
 			{
 				quit = true;
 			}
-			//TODO:Pre-Compute these outside of the loop.
-			camera.UpdateCamera(&event);
-//			SDL_WarpMouseInWindow(window.GetWindow(),(int)(WIDTH/2),(int)(HEIGHT/2));
-	}
+			SDL_WarpMouseInWindow(window.GetWindow(),(int)HALF_WIDTH,(int)HALF_HEIGHT);
+			camera.UpdateCamera(&event,HALF_WIDTH,HALF_HEIGHT);
+		}
 		//2.Logic
-	
 		//3.Rendering
 	        SDL_RenderClear(window.GetRenderer());
 		SDL_RenderCopy(window.GetRenderer(), window.GetTexture(), NULL, NULL);
 	        SDL_RenderPresent(window.GetRenderer());
-       	}
-	
+       		for(unsigned int i = 0;i < objects.size();i++)
+		{
+			rasterizer.RasterizeObjMesh(camera.GetMatrix(),objects.at(i));
+		}
+		rasterizer.ClearZBuffer();
+		deltaTime = (float)((currentTime - lastTime)/1000.0f);
+		lastTime = currentTime;
+		
+	}
+
 	window.destroy(); 
 	SDL_Quit(); 
 	return 0;
